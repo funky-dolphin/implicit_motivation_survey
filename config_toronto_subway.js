3ded95d2f03327768da58098f5b239622474ab00
Nicholas Brereton <nicholasrbrereton@gmail.com>
2025-11-24 16:22:11 -0500

    Config edited

---
+++ config_toronto_subway.js
@@ -0,0 +1,93 @@
+const brands = [
+    { name: "A&W", img: "img/aw.png" },
+    { name: "Chipotle", img: "img/chipotle.png" }, 
+    { name: "Freshii", img:"img/freshii.png"}, 
+    { name: "Harveys", img:"img/harveys.png"},
+    { name: "McDonalds", img: "img/mcdonalds.png"},
+    { name: "Subway", img:"img/Subway.png"},
+    { name: "Tim Hortons", img: "img/Tim_hortons.png" },
+    { name: "Wendys", img: "img/wendys.png" }
+  ];
+
+const category = "Fast Food"
+
+const attributes = [
+"adventure",
+"freedom",
+"courage",
+"impulsiveness",
+"discovery",
+"escape",
+"be unconventional",
+"liberation",
+"pride",
+"perfection",
+"recognition",
+"success",
+"achievement",
+"doing my best",
+"empowerment",
+"improve myself",
+"discipline",
+"control",
+"reason",
+"efficiency",
+"simplicity",
+"be practical",
+"consistent",
+"be savvy",
+"safety",
+"security",
+"caring",
+"closeness",
+"sense of harmony",
+"peace of mind",
+"belonging",
+"trust",
+"joy",
+"spoil myself",
+"carefreeness",
+"relief",
+"comfort",
+"relaxation",
+"be at ease with myself",
+"satisfaction",
+"excitement",
+"zest for life",
+"vitality",
+"fun",
+"playfulness",
+"curiosity",
+"be on trend",
+"energy"];
+
+
+const pretest_attributes = [
+  "Hot", 
+  "Cold", 
+  "Dangerous",
+  "Refreshing", 
+  "Cute",
+  "Soft"
+];
+
+
+const pretest_images = [
+  {name: "Cat", img: "pretest_img/pretest_cat.png", correct: ["Cute", "Soft"]},
+  {name: "Fire", img:"pretest_img/pretest_fire.png", correct:["Dangerous", "Hot"]}, 
+  {name: "Icecube", img: "pretest_img/pretest_icecube.png", correct:["Cold", "Refreshing"]}
+]
+
+const pretest_attributes_multiple = ["Hot", "Cold", "Dangerous", "Refreshing", "Cute", "Soft", "Travel", "Excitement", "Precise", "Time", "Deep", "Wet"]
+
+const pretest_images_multiple = [
+  {name: "Cat", img: "pretest_img/pretest_cat.png", correct: ["Cute", "Soft"]},
+  {name: "Fire", img:"pretest_img/pretest_fire.png", correct:["Dangerous", "Hot"]}, 
+  {name: "Icecube", img: "pretest_img/pretest_icecube.png", correct:["Cold", "Refreshing"]}, 
+  {name: "Driving", img: "pretest_img/pretest_driving.png", correct:["Travel", "Excitement"]}, 
+  {name: "Ocean", img: "pretest_img/pretest_ocean.png", correct:["Deep", "Wet"]},
+  {name: "Clock", img: "pretest_img/pretest_clock.png", correct:["Precise", "Time"]}
+]
+
+
+const survey_name = "Pringles Trial FCBNY"
